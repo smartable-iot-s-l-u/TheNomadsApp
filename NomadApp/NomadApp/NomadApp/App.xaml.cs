b40432a96dcd899d889aa006cf64f06d198ccd84
leylaklar soldu kalbinde <jordiv77@hotmail.com>
2019-05-29 13:28:50 +0200

    Inicio carpetas MVVM

---
+++ NomadApp/NomadApp/NomadApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace NomadApp
+{
+    using NomadApp.Views;
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PrincipalPage(); //La app arranca por la PrincipalPage
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
